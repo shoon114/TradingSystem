8cc5b443441b729cbd3946df6c3d17c2a9b11ebb
Sanghoon Lee <shoon114@icloud.com>
2026-07-03 15:00:42 +0900

    [testing] OrderScheduler scheduleOrder/processDueOrders TC 추가
    OrderScheduler(StockBrocker*, ILogger*)가 예약 큐를 관리하는
    기본 동작을 검증하는 TC:
    - 예약 실행시각에 도달하면(now >= executeTime) 큐에 저장된 주문을
      실행하고 로그를 남긴다.
    - 아직 도달하지 않았다면(now < executeTime) 주문을 실행하지 않고
      로그도 남기지 않는다.
    
    실제 스레드/타이머 구현은 담당자 몫이며, 테스트는 processDueOrders에
    현재 시각을 외부에서 주입해 시간 의존성 없이 검증한다.

--- TestOrderScheduler.cpp
+++ TestOrderScheduler.cpp
@@ -1,8 +1,11 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "mock_stock_brocker.h"
+#include "MockLogger.h"
 #include "OrderCommand.h"
+#include "OrderScheduler.h"
 #include <memory>
+#include <chrono>
 
 using ::testing::Return;
 
@@ -26,4 +29,40 @@ TEST(OrderCommandTest, SellOrder_Execute_DelegatesToBrockerSell) {
 	EXPECT_CALL(brocker, sell("005930", 70000, 10)).Times(1);
 
 	order.execute(brocker);
+}
+
+// OrderScheduler::scheduleOrder / processDueOrders
+// - 예약된 실행시각(executeTime)에 도달하면(now >= executeTime), 큐에 저장된 주문을
+//   실행하고 로그를 남겨야 한다.
+TEST(OrderSchedulerTest, ProcessDueOrders_ExecutesAndLogsOrder_WhenTimeReached) {
+	MockStockBrocker brocker;
+	MockLogger logger;
+	OrderScheduler scheduler(&brocker, &logger);
+
+	auto executeTime = std::chrono::system_clock::now();
+	auto order = std::make_shared<BuyOrder>("005930", 70000, 10);
+	scheduler.scheduleOrder(order, executeTime);
+
+	EXPECT_CALL(brocker, buy("005930", 70000, 10)).Times(1);
+	EXPECT_CALL(logger, log(::testing::_)).Times(1);
+
+	scheduler.processDueOrders(executeTime);
+}
+
+// - 예약된 실행시각에 아직 도달하지 않았다면(now < executeTime), 주문을 실행하지 않고
+//   로그도 남기지 않아야 한다.
+TEST(OrderSchedulerTest, ProcessDueOrders_DoesNotExecuteOrder_BeforeScheduledTime) {
+	MockStockBrocker brocker;
+	MockLogger logger;
+	OrderScheduler scheduler(&brocker, &logger);
+
+	auto now = std::chrono::system_clock::now();
+	auto executeTime = now + std::chrono::hours(1);
+	auto order = std::make_shared<BuyOrder>("005930", 70000, 10);
+	scheduler.scheduleOrder(order, executeTime);
+
+	EXPECT_CALL(brocker, buy(::testing::_, ::testing::_, ::testing::_)).Times(0);
+	EXPECT_CALL(logger, log(::testing::_)).Times(0);
+
+	scheduler.processDueOrders(now);
 }
