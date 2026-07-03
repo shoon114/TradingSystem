e1541654dcdd98a47e6eaab3f8e4a181e308ad56
Sanghoon Lee <shoon114@icloud.com>
2026-07-03 14:59:41 +0900

    [testing] OrderScheduler SellOrder TC 추가
    SellOrder::execute(brocker) 호출 시 brocker.sell(종목코드, 가격, 수량)이
    호출되는지 검증하는 기본 TC.

--- TestOrderScheduler.cpp
+++ TestOrderScheduler.cpp
@@ -16,3 +16,14 @@ TEST(OrderCommandTest, BuyOrder_Execute_DelegatesToBrockerBuy) {
 
 	order.execute(brocker);
 }
+
+// IOrderCommand::SellOrder
+// - execute(brocker) 호출 시 brocker.sell(종목코드, 가격, 수량)이 호출되어야 한다.
+TEST(OrderCommandTest, SellOrder_Execute_DelegatesToBrockerSell) {
+	MockStockBrocker brocker;
+	SellOrder order("005930", 70000, 10);
+
+	EXPECT_CALL(brocker, sell("005930", 70000, 10)).Times(1);
+
+	order.execute(brocker);
+}
